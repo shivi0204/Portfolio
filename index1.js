307df3994ef3d63f6ceca57c7a7adbc861bd0b15
Shivani <104577409+shivi0204@users.noreply.github.com>
2024-07-25 23:01:27 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
+
+
